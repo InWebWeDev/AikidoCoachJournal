d857a2c654735dd5cf21bfc2bf7e3e29420da74e
Maxim <maxim.kudryakov@internet.ru>
2023-11-25 16:57:09 +0500

    Authorization 1.0

---
+++ src/components/ProfilePage.tsx
@@ -0,0 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
+const ProfilePage = () => {
+  const navigate = useNavigate();
+ 
+  return (
+    <>
+      <h1>Profile</h1>
+    </>
+  )
+}
+
+export default ProfilePage;
